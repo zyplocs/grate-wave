5a864df8cf3ef31876a05bec7c97f0de8cd0fd7b
zyplocs <zyplocked@gmail.com>
2025-12-16 04:07:37 -0600

    Initial Commit

---
+++ GrateWave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrateWave
+//
+//  Created by Eli J on 12/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrateWave/GrateWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrateWaveApp.swift
+//  GrateWave
+//
+//  Created by Eli J on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GrateWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
